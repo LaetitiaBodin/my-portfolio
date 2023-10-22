5de6527d9ad0cf7d6364c2b5445b6dbb2b420744
LaetitiaBodin <laetitia.bodin.webdev@gmail.com>
2023-10-23 00:08:28 +0200

    update: 2023.10.23

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ export const Footer = () => {
                 <a href='https://github.com/LaetitiaBodin?tab=repositories' target='_blank' rel='noopener noreferrer'>
                     <ImgGithub/>
                 </a>
-                <a href={`${process.env.PUBLIC_URL}/assets/docs/cv--dev-web-junior--laetitia-bodin.pdf`} target='_blank' rel='noopener noreferrer'>
+                <a href={`${process.env.PUBLIC_URL}/assets/docs/cv--dev--laetitia-bodin.pdf`} target='_blank' rel='noopener noreferrer'>
                     <ImgResume/>
                 </a>
             </div>
@@ -25,7 +25,7 @@ const ImgGithub = () => {
                 0-1.06a3.2 3.2 0 0 1 2.44 1.64h0a3.42 3.42 0 0 0 4.66 1.32h0a3.38 3.38 0 0 1 1-2.14c-3.54-.4-7.26-1.78-7.26-7.9a6.22 6.22 0 0 1 1.64-4.3 5.76 
                 5.76 0 0 1 .16-4.24S9 6.2 12 8.26a15.08 15.08 0 0 1 8 0c3.06-2.08 4.4-1.64 4.4-1.64a5.76 5.76 0 0 1 .16 4.24 6.17 6.17 0 0 1 1.64 4.3c0 
                 6.14-3.74 7.5-7.3 7.9a3.79 3.79 0 0 1 1.08 3v4.4a.78.78 0 0 0 .78.8.73.73 0 0 0 .25 0A16 16 0 0 0 32 15.96c0-8.837-7.163-16-16-16z"/>
-            </svg>
+        </svg>
     )
 }
 
@@ -37,6 +37,6 @@ const ImgResume = () => {
             <path className='rect_l' d="M11.67 25.6h11.66v2.13H11.67zm0-4.27h11.66v2.14H11.67zm0-4.26h11.66v2.13H11.67zm0-4.27h11.66v2.13H11.67zm0-4.27h11.66v2.14H11.67z"/>
             <path className='rect_s' d="M4.67 17.07h4.66v2.13H4.67zm0-4.27h4.66v2.13H4.67z"/>
             <path className='pic' d="M9.33 8.53A1.59 1.59 0 0 0 8.19 7a1.56 1.56 0 0 0 .56-1.17 1.76 1.76 0 0 0-3.5 0A1.56 1.56 0 0 0 5.81 7a1.61 1.61 0 0 0-1.14 1.49v2.14h4.66z"/>
-            </svg>
+        </svg>
     )
 }
